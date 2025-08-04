40b1afa84a95bc94996533170e5146d73da3aab6
qig <ashrafkoh@gmail.com>
2025-08-04 19:49:30 +0300

    removed runtime migrations

--- sales_api/main.py
+++ sales_api/main.py
@@ -10,7 +10,7 @@ from . import models, schemas, database
 #     return {"Hello": "World"}
 
 
-models.Base.metadata.create_all(bind=database.engine)
+# models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Sales API")
 
